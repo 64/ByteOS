43ac772a096f2539709dd79ad4f554119d2cc38f
Matthew Taylor <mstaveleytaylor@gmail.com>
2016-10-11 22:49:23 +0100

    Initialize key state caching for PS/2 keyboard driver

--- kernel/drivers/ps2/kbd.c
+++ kernel/drivers/ps2/kbd.c
@@ -2,17 +2,100 @@
 #include <isr.h>
 #include <klog.h>
 #include <io.h>
-#include <algs/queue.h>
+#include <drivers/pit.h>
+#include <string.h>
 
-#define KBD_COMMAND 0x64
+#define KBD_DATA  0x60
+#define KBD_STATUS 0x64
+#define KBD_RELEASE_MASK 0x80
+#define KBD_OFFSET_LSHIFT (keyboard_us_offsets[0x2A])
+#define KBD_OFFSET_RSHIFT (keyboard_us_offsets[0x36])
+#define KBD_OFFSET_LCTRL (keyboard_us_offsets[0x1D])
+#define KBD_OFFSET_LALT (keyboard_us_offsets[0x38])
+#define KBD_OFFSET_CAPSLOCK (keyboard_us_offsets[0x3A])
+#define KBD_SET_KEY_OFFSET(offset) key_states.data[(offset) / 16] |= (uint16_t)(1 << ((offset) % 16))
+#define KBD_CLEAR_KEY_OFFSET(offset) key_states.data[(offset) / 16] &= (uint16_t)(~(1 << ((offset) % 16)))
 #define KBD_LED_SCROLL (1 << 0)
 #define KBD_LED_NUM (1 << 1)
 #define KBD_LED_CAPS (1 << 2)
 
+extern const uint8_t keyboard_us_offsets[];
+extern const uint8_t keyboard_us_uppercase[];
+extern const uint8_t keyboard_us_lowercase[];
+
+kbd_state key_states;
+
 static inline void keyboard_wait(void) {
-	while (io_inportb(KBD_COMMAND) & 2)
+	while (io_inportb(KBD_STATUS) & 2)
 		;
 }
+
+static inline uint8_t keyboard_scancode_to_offset(uint8_t scancode) {
+	return keyboard_us_offsets[scancode];
+}
+
+static inline uint8_t keyboard_scancode_to_char(uint8_t scancode) {
+	return keyboard_us_uppercase[scancode];
+}
+
+void keyboard_set_led(uint8_t flags) {
+	if (flags == 0)
+		return;
+	flags &= 0x07; // Only use last 3 bits
+	io_outportb(KBD_DATA, 0xED); keyboard_wait();
+	io_outportb(KBD_DATA, flags); keyboard_wait();
+	uint8_t temp = 0;
+	while (temp != 0xFA) {
+		temp = io_inportb(KBD_DATA);
+		if (temp == 0xFE) {
+			io_outportb(KBD_DATA, 0xED); keyboard_wait();
+			io_outportb(KBD_DATA, flags); keyboard_wait();
+		}
+	}
+
+}
+
+void keyboard_handler(struct regs *r) {
+	keyboard_wait();
+	uint8_t scancode = io_inportb(KBD_DATA);
+	irq_ack(r->int_no - 32);
+	if (scancode & KBD_RELEASE_MASK) {
+		uint8_t offset = keyboard_scancode_to_offset(scancode - KBD_RELEASE_MASK);
+		KBD_CLEAR_KEY_OFFSET(offset);
+	} else {
+		uint8_t offset = keyboard_scancode_to_offset(scancode);
+		KBD_SET_KEY_OFFSET(offset);
+		printf("%c", keyboard_scancode_to_char(scancode));
+	}
+}
+
+void keyboard_install(void) {
+	klog_notice("Initializing PS/2 keyboard driver...\n");
+	memset(&key_states, 0, sizeof(key_states));
+	uint8_t test = 0;
+	while(((test = io_inportb(KBD_STATUS)) & 1) == 1)
+		io_inportb(KBD_DATA);
+	irq_install_handler(1, keyboard_handler);
+	keyboard_set_led(KBD_LED_CAPS | KBD_LED_SCROLL | KBD_LED_NUM);
+	while(pit_timer_wait_ms(200), 1) { printf("%d\n", key_states.data[0]); }
+}
+
+// Offsets for each key on the keyboard
+const uint8_t keyboard_us_offsets[128] = {
+	0, 0, 1, 2, 3, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0
+};
 
 const uint8_t keyboard_us_lowercase[128] = {
 	0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	     /* 9 */
@@ -91,43 +174,3 @@ const uint8_t keyboard_us_uppercase[128] = {
 	0,                                                     /* F12 */
 	0,                                                     /* everything else */
 };
-
-static inline uint8_t keyboard_scancode_convert(uint8_t scancode) {
-	return keyboard_us_uppercase[scancode];
-}
-
-void keyboard_set_led(uint8_t flags) {
-	if (flags == 0)
-		return;
-	flags &= 0x6F; // Only use last 3 bits
-	io_outportb(KBD_COMMAND, 0x8E); keyboard_wait();
-	io_outportb(KBD_COMMAND, flags); keyboard_wait();
-}
-
-void keyboard_handler(struct regs *r) {
-	keyboard_wait();
-	uint8_t scancode = io_inportb(0x60);
-	irq_ack(r->int_no - 32);
-
-	if (scancode & 0x80)
-		;
-	else
-		printf("%c", keyboard_scancode_convert(scancode));
-}
-
-queue_int *kbd_cmdq;
-
-void keyboard_install(void) {
-	klog_notice("Initializing PS/2 keyboard driver...\n");
-	uint8_t test = 0;
-	while(((test = io_inportb(KBD_COMMAND)) & 1) == 1) {
-	   io_inportb(0x60);
-	}
-	kbd_cmdq = queue_int_create(20);
-	irq_install_handler(1, keyboard_handler);
-	keyboard_set_led(KBD_LED_CAPS | KBD_LED_SCROLL | KBD_LED_NUM);
-}
-
-void keyboard_cleanup(void) {
-	queue_int_free(kbd_cmdq);
-}
--- kernel/include/drivers/ps2/kbd.h
+++ kernel/include/drivers/ps2/kbd.h
@@ -2,4 +2,8 @@
 
 #include <stdint.h>
 
+typedef struct {
+	uint16_t data[8];
+} kbd_state;
+
 void keyboard_install(void);
