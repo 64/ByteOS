789922b6b9eccbf8a854ef1afdf2b8d1b3e50fe5
Matthew Taylor <mstaveleytaylor@gmail.com>
2017-09-03 23:53:46 +0100

    Use typeof in macros

--- include/kernel/util.h
+++ include/kernel/util.h
@@ -1,7 +1,23 @@
 #pragma once
 
-#define MIN(x, y) ((x) > (y) ? (y) : (x))
-#define MAX(x, y) ((x) < (y) ? (y) : (x))
-#define ALIGNUP(val, align) (((val) + (align - 1)) & -(align))
-#define ISALIGN_POW2(val, align) (((val) & ((align) - 1)) & ((val) - 1) == 0)
+#define MIN(x, y) ({ \
+	const typeof((x)) _x = (x); \
+	const typeof((y)) _y = (y); \
+	_x > _y ? _y : _x; })
+
+#define MAX(x, y) ({ \
+	const typeof((x)) _x = (x); \
+	const typeof((y)) _y = (y); \
+	_x > _y ? _x : _y; })
+
+#define ALIGNUP(val, align) ({ \
+	const typeof((val)) _val = (val); \
+	const typeof((align)) _align = (align); \
+	(_val + (_align - 1)) & -_align; })
+
+#define ISALIGN_POW2(val, align) ({ \
+	const typeof((val)) _val = (val); \
+	const typeof((align)) _align = (align); \
+	(_val & (_align - 1)) & (_val - 1) == 0; })
+
 #define UNUSED(x) __attribute__((unused)) x
